2886a335b7700dd72bc8bcbb0cd99db28ca7c93a
user <user@example.org>
2024-10-08 23:18:41 -0700

    bug fix, better error handling, multithreaded reverse dns resolution, pretty printing of console output

--- purview_azure_geoip_enrich_logs.py
+++ purview_azure_geoip_enrich_logs.py
@@ -1,14 +1,20 @@
 import argparse
 import json
 import os
-from tkinter.constants import FALSE
-
+import sys
 import geoip2.database
 import geoip2.errors
 import pandas as pd
-import socket
+from rdns_reaper import RdnsReaper
+from yaspin import yaspin
+from yaspin.spinners import Spinners
+from colorama import init, Fore
+
+# set auto reset for colorama print messages
+init(autoreset=True)
 
 def de_dupe_ips(dataframe):
+    """get list of unique ips from dataframe and deduplicate"""
     data = []
     unique_ips = []
     for index, row in dataframe.iterrows():
@@ -18,38 +24,30 @@ def de_dupe_ips(dataframe):
     return unique_ips
 
 
-# noinspection PyBroadException
 def ip2dns(ipaddresses):
-    ip2dnsdict = {}
-    for ip in ipaddresses:
-        try:
-            hostname = socket.gethostbyaddr(ip)
-            ip2dnsdict[ip] = hostname[0]
-        except:
-            hostname = "HOSTNOTFOUND"
-            ip2dnsdict[ip] = hostname
-    return ip2dnsdict
+    """reverse IP lookup using dnsreaper"""
+    rdr = RdnsReaper(limit_to_rfc1918=False, concurrent=200, unresolvable=r"HOSTNOTFOUND")
+    rdr.add_ip_list(ipaddresses)
+    rdr.resolve_all()
+    return rdr.get_dict()
 
 
 def geoip_lookup(json_data):
-    # TODO Add argument and parsing for marking known registered devices as low risk. Sample JSON:
-    # "DeviceProperties": [
-    #     {"Name": "Id", "Value": "<guid>"},
-    #     {"Name": "DisplayName", "Value": "<computername>"},
-    #     {"Name": "OS", "Value": "Windows"},
-    #     {"Name": "BrowserType", "Value": "Other"},
-    #     {"Name": "TrustType", "Value": "1"},
-    #     {"Name": "SessionId", "Value": "<guid>"}
-    # ],
-    # Parse the JSON data
-    data = json.loads(json_data)
+    """perform geoip lookup with offline maxmind database"""
+    try:
+        data = json.loads(json_data)
+    except Exception as ejson:
+        print(Fore.YELLOW + "[*] " + Fore.RESET + "Failed to parse JSON in geoip_lookup")
+        print(Fore.YELLOW + "[*] " + Fore.RESET + "Exception: %s" % str(ejson))
 
     # Extract the ClientIP value
-    client_ip = data.get("ClientIP")
+    try:
+        client_ip = data.get("ClientIP")
+    except Exception as eclientip:
+        print(Fore.YELLOW + "[*] " + Fore.RESET + "Failed to extract ClientIP value")
+        print(Fore.YELLOW + "[*] " + Fore.RESET + "Exception: %s" % str(eclientip))
 
-    # TODO see above comment; Extract DeviceID
-    # device_id = data.get("Id")
-
+    # TODO refactor
     if client_ip:
         # Perform GeoIP lookup
         reader = geoip2.database.Reader("maxmind-bins/db/GeoLite2-City.mmdb")
@@ -62,31 +60,36 @@ def geoip_lookup(json_data):
             fulllocation = str(city) + ", " + str(country)
             # print(fulllocation, client_ip)
             return fulllocation, client_ip
-        except geoip2.errors.AddressNotFoundError:
-            # print(client_ip)
+        except geoip2.errors.AddressNotFoundError as egeoip:
+            print(Fore.YELLOW + "[*] " + Fore.RESET + "Exception with geoip lookup for " + client_ip)
+            print(Fore.YELLOW + "[*] " + Fore.RESET + "Exception: %s" % str(egeoip))
             return "NOT_FOUND", client_ip
         finally:
             reader.close()
     else:
-        print("NOT_FOUND")
         return "NOT_FOUND", "NOT_FOUND"
 
 
 def enrich_df(dataframe, ip2dnslookkupdict):
+    """enrich dataframe with reverse dns and geoip information"""
     for index, row in dataframe.iterrows():
         data = geoip_lookup(row["AuditData"])
         df.at[index, "GeoIPLocation"] = str(data[0])
-        df.at[index, "HostName"] = ip2dnslookkupdict[str(data[1])]
+        try:
+            df.at[index, "HostName"] = ip2dnslookkupdict[str(data[1])]
+        except KeyError as keye:
+            print(Fore.YELLOW + "[*] " + Fore.RESET + "IP2DNS dictionary key lookup failure.")
+            print(Fore.YELLOW + "[*] " + Fore.RESET + "Exception: %s" % str(keye))
         df.at[index, "IPAddress"] = str(data[1])
         df.at[index, "MultiGeoIPLink"] = "https://www.iplocation.net/ip-lookup" #TODO temporary until broader api support
 
 
-# argument handling
+# basic argument handling
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', help="Path to the csv file to enrich.")
 args = parser.parse_args()
 
-# open the file; fixed file encoding parsing issues by forcing utf8 and back slash escaping
+# open the file; fixed file encoding parsing issues by forcing utf8 and backslash escaping
 file_encoding = 'utf8'  # set file_encoding to the file encoding (utf8, latin1, etc.)
 input_fd = open(args.filename, encoding=file_encoding, errors='backslashreplace')
 df = pd.read_csv(input_fd)
@@ -101,17 +104,33 @@ df['MultiGeoIPLink'] = None
 df = df.loc[df['RecordType'] == 15]
 
 # do DNS lookups once
-ips2resolve = de_dupe_ips(df)
-ip2dnsdictionary = ip2dns(ips2resolve)
+with yaspin(Spinners.hearts, text="Performing DNS reverse lookups..."):
+    ips2resolve = de_dupe_ips(df)
+    ips2resolve = [i for i in ips2resolve if i] # remove empty strings with list comprehension
+    ip2dnsdictionary = ip2dns(ips2resolve)
+print(Fore.GREEN + "[*] " + Fore.RESET + "Completed DNS reverse lookups.")
 
 # enrich data with geoip info
+print(Fore.CYAN + "[*] " + Fore.RESET + "Enriching data...")
 enrich_df(df, ip2dnsdictionary)
+print(Fore.GREEN + "[*] " + Fore.RESET + "Data enriched.")
 
 # write out enriched file
-newfile = args.filename[:-4] + "_geoip_enriched.xlsx"
-df.to_excel(newfile, index=False)
-print("GeoIP enriched log data written to " + newfile)
+print(Fore.CYAN + "[*] " + Fore.RESET + "Writing Excel file...")
+try:
+    newfile = args.filename[:-4] + "_geoip_enriched.xlsx"
+    df.to_excel(newfile, index=False)
+    print(Fore.GREEN + "[*] " + Fore.RESET + "GeoIP enriched log data written to " + newfile)
+except Exception as e:
+    print(Fore.RED + "[*] " + Fore.RESET + "Can't write file. Is a file in the same location already open in Excel?")
+    print ("Exception: %s" % str(e))
+    sys.exit(1)
 
 # open it in default app
-print("Opening file...")
-os.startfile(newfile, 'open')
+try:
+    print(Fore.CYAN + "[*] " + Fore.RESET + "Opening Excel file " + newfile)
+    os.startfile(newfile, 'open')
+except Exception as e:
+    print(Fore.RED + "[*] " + Fore.RESET + "Can't open file. Something went very wrong.")
+    print ("Exception: %s" % str(e))
+    sys.exit(1)
