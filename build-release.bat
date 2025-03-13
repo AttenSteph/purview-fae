93b472d3acdb848bfa0bfda0960d2b8717dd2d6a
user <user@example.org>
2025-03-12 21:01:42 -0700

    more updates

--- build-release.bat
+++ build-release.bat
@@ -1,1 +1,1 @@
-python -m nuitka --onefile --windows-console-mode=force .\purview_azure_geoip_enrich_logs.py
+python -m nuitka --onefile --enable-plugin=tk-inter --windows-console-mode=force .\purview-fae.py
